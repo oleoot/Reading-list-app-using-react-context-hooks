7223bfe3b5614ab11b5e99e4ed6d950c7a0e8fa6
oleoot <oleoot28@gmail.com>
2020-06-11 21:26:31 +0300

    Added new book components, displaying them on the page + some more styles

--- reading-list-react-context-hooks/src/App.js
+++ reading-list-react-context-hooks/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookContextProvider from './contexts/BookContext';
 import Navbar from './components/Navbar';
+import Booklist from './components/Booklist';
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <Navbar />
+        <Booklist />
       </BookContextProvider>
     </div>
   );
